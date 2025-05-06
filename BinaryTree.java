ae4258c5c2090e6347ce423149a3e9f6f7a7e775
Lucas Alexandre <lucasparaoif@gmail.com>
2025-05-06 11:04:50 -0300

    adiciona arvore binária avulsa com percursos recursivos

---
+++ BinaryTree.java
@@ -0,0 +1,62 @@
+public class BinaryTree {
+    
+    private Node root;
+
+    public BinaryTree() {
+        this.root = null;
+    }
+
+    public BinaryTree(Node root) {
+        this.root = root;
+    }
+
+    public Node getRoot() {
+        return this.root;
+    }
+
+    public void preOrder() {
+        preOrder(this.root);
+        System.out.println();
+    }
+
+    public void preOrder(Node node) {
+
+        if (node != null) {
+            System.out.print(node.getValue() + " ");
+            preOrder(node.getLeft());
+            preOrder(node.getRight());
+        }
+        
+    }
+
+    public void inOrder() {
+        inOrder(this.root);
+        System.out.println();
+    }
+
+    public void inOrder(Node node) {
+
+        if (node != null) {
+            inOrder(node.getLeft());
+            System.out.print(node.getValue() + " ");
+            inOrder(node.getRight());
+        }
+
+    }
+
+    public void postOrder() {
+        postOrder(this.root);
+        System.out.println();
+    }
+
+    public void postOrder(Node node) {
+
+        if (node != null) {
+            postOrder(node.getLeft());
+            postOrder(node.getRight());
+            System.out.print(node.getValue() + " ");
+        }
+
+    }
+
+}
---
+++ BinaryTreeMain.java
@@ -0,0 +1,22 @@
+public class BinaryTreeMain {
+    public static void main(String[] args) {
+
+        Node node9 = new Node(9);
+        Node node8 = new Node(8);
+        Node node7 = new Node(7);
+
+        Node node6 = new Node(6);
+        Node node5 = new Node(5, node8, node9);
+        Node node4 = new Node(4, null, node7);
+
+        Node node3 = new Node(3, node5, node6);
+        Node node2 = new Node(2, node4, null);
+        Node node1 = new Node(1, node2, node3);
+
+        BinaryTree bt1 = new BinaryTree(node1);
+        bt1.preOrder();
+        bt1.inOrder();
+        bt1.postOrder();
+        
+    }
+}
